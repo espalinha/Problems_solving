7ace5dac521fb4e858a4fcd476349f0cfcc16801
Espala <matheusespalaor14@gmail.com>
2023-10-22 21:21:13 -0300

    Primeiros exercicios

--- GFG/GAS/cria.py
+++
@@ -1,7 +0,0 @@
-import random
-def main():
-    print(100)
-    for _ in range(100):
-        print(random.randint(0, 50))
-        
-main()
--- GFG/MN/cria.py
+++
@@ -1,8 +0,0 @@
-import random
-
-def main():
-    lista = [i for i in range(1000)]
-    random.shuffle(lista)
-    for i in lista:
-        print(i)
-main()
